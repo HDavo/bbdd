8b1126cf9d90475d4f7b4226abe5c4475b777c17
Dav <dabghos@outlook.com>
2023-01-21 22:15:32 +0100

    Cambios

---
+++ pl/t12/ejs_funciones_t12.sql
@@ -0,0 +1,11 @@
+--15 Crear una función que sume dos números.
+
+--16 Crear una función que visualice una cadena al revés.
+
+/* 17 Escribe una función que reciba una fecha y devuelva el año, en número, correspondiente a esa fecha. */
+
+
+/* 18-Escribir una función que devuelva el valor con IVA de una cantidad que se pasara como primer parámetro. La función podrá recoger un segundo parámetro, que será el tipo de IVA siendo el valor por defecto 16. /*
+
+/* 19- Utilizar la función anterior para obtener el salario de
+los empleados con el iva de la función. */
