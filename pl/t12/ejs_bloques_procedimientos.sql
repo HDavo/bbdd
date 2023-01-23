b778612f54407addc253539fa1b314c3898eaa1f
HDavo <dabghos@hotmail.com>
2023-01-24 00:15:23 +0100

    Final bloques_procedimientos

--- pl/t12/ejs_bloques_procedimientos.sql
+++ pl/t12/ejs_bloques_procedimientos.sql
@@ -21,6 +21,18 @@ EXECUTE(45);
 --BLOQUES
 
 
+DECLARE
+IS
+	RESULTADO NUMBER;
+	RADIO NUMBER;
+	OTRO NUMBER;
+	PI NUMBER;
+BEGIN
+	SELECT () FROM DUAL;
+END;
+/
+
+
 /* 2.	Obtener el salario del empleado 7839 de la tabla EMPLE. */
 
 CREATE OR REPLACE PROCEDURE OBTENER_SALARIO(X_EMPLE NUMBER)
@@ -34,6 +46,14 @@ END;
 EXECUTE OBTENER_SALARIO(7839);
 
 --BLOQUES
+
+DECLARE
+	V_SALARIO EMPLE.SALARIO%TYPE;
+BEGIN
+	SELECT SALARIO INTO V_SALARIO FROM EMPLE WHERE EMP_NO = X_EMPLE;
+	DBMS_OUTPUT.PUT_LINE('El salario correspondiente al número de empleado introducido es '||V_SALARIO);
+END;
+/
 
 /* 3.	Mostrar el nombre de la asignatura con código 7 de la tabla ASIGNATURAS. */
 
@@ -49,6 +69,14 @@ EXECUTE(7);
 
 --BLOQUES
 
+DECLARE
+	V_ASIGNATURA ASIGNATURAS.NOMBRE%TYPE;
+BEGIN
+	SELECT NOMBRE INTO V_ASIGNATURA FROM ASIGNATURAS WHERE COD = NUM;
+	DBMS_OUTPUT.PUT_LINE('La asignatura correspondiente al código introducido es: '||V_ASIGNATURA);
+END;
+/
+
 
 /* 4.	Visualizar el nombre de la tabla PROFESORES, introduciendo el DNI por teclado a través de una variable de sustitución. */
 
@@ -63,23 +91,146 @@ END;
 
 --BLOQUES
 
+DECLARE
+	V_NOMBRE PROFESORES.APELLIDOS%TYPE;
+BEGIN
+	SELECT APELLIDOS INTO V_NOMBRE FROM PROFESORES WHERE DNI = X_DNI;
+	DBMS_OUTPUT.PUT_LINE('El DNI'||X_DNI||' corresponde a '||V_NOMBRE);
+end;
+/
 
 /* 5.	A través de variables visualice el departamento VENTAS de la tabla DEPART con el siguiente mensaje ‘El departamento nº X esta en XXX’. */
 
+CREATE OR REPLACE PROCEDURE VER_VENTAS(COSA VARCHAR2)
+IS
+	V_DEP DEPART.DNOMBRE%TYPE;
+	V_LOCALIDAD DEPART.LOC%TYPE;
+BEGIN
+	SELECT DEPT_NO, LOC INTO V_DEP, V_LOCALIDAD FROM DEPART WHERE DNOMBRE = COSA;
+	DBMS_OUTPUT.PUT_LINE('El departamento nº'||V_DEP||' está en '||V_LOCALIDAD);
+END;
+/
+
+EXECUTE VER_VENTAS('VENTAS');
+
+--BLOQUES
+
+DECLARE
+	V_DEP DEPART.DNOMBRE%TYPE;
+	V_LOCALIDAD DEPART.LOC%TYPE;
+BEGIN
+	SELECT DEPT_NO, LOC INTO V_DEP, V_LOCALIDAD FROM DEPART WHERE DNOMBRE = COSA;
+	DBMS_OUTPUT.PUT_LINE('El departamento nº'||V_DEP||' está en '||V_LOCALIDAD);
+END;
+/
 
 /* 6.	Insertar en la tabla EMPLE un empleado con código 9999 asignado directamente en la variable,
 apellido ‘PEREZ’, oficio ‘ANALISTA’ y código del departamento al que pertenece 10.*/
 
+
+CREATE OR REPLACE PROCEDURE PONER_EMPLEADO(NOM VARCHAR, PUESTO VARCHAR, DEPA NUMBER, )
+IS
+	V_COD EMPLE.EMP_NO%TYPE DEFAULT 9999;
+BEGIN
+	INSERT INTO EMPLE (APELLIDO, OFICIO, DEPT_NO) VALUES (NOM, PUESTO, DEPA);
+END;
+/
+
+
+--BLOQUES
+
+DECLARE
+	V_COD EMPLE.EMP_NO%TYPE DEFAULT 9999;
+BEGIN
+	INSERT INTO EMPLE (APELLIDO, OFICIO, DEPT_NO) VALUES (NOM, PUESTO, DEPA);
+END;
+/
+	
 /* 7.	Incrementar el salario en la tabla EMPLE en 200€ a todos los trabajadores que sean ‘ANALISTAS’, mediante un bloque anónimo PL, asignando dicho valor a una variable declarada%TYPE.
  */
+
+--BLOQUES 
+DECLARE
+BEGIN 
+	UPDATE EMPLE SET SALARIO = SALARIO+200 WHERE OFICIO = 'ANALISTA';
+END;
+/
+
+CREATE OR REPLACE PROCEDURE AUMENTO_SALDO(V_OFICIO VARCHAR)
+IS
+BEGIN
+	UPDATE EMPLE SET SALARIO = SALARIO+200 WHERE OFICIO = V_OFICIO;
+END;
+/
 
 /* 8.	Borrar un registro de la tabla EMPLE, existiendo en el bloque una variable PL y otra de sustitución.
  */
 
+DECLARE
+BEGIN 
+	DELETE FROM EMPLE WHERE EMP_NO = &NUMERO_EMP;
+END:
+/
+
+CREATE OR REPLACE PROCEDURE BORRADO(COSA NUMBER)
+IS
+BEGIN
+	DELETE FROM EMPLE WHERE EMP_NO = COSA;
+END;
+/
+
 /* 9.	Suprimir de la tabla ASIGNATURAS aquellos que tengan un código mayor a cinco utilizando los atributos del cursor SQL%ROWCOUNT. Que muestre cuantas filas ha borrado. 
  */
+
+DECLARE
+BEGIN
+	DELETE FROM ASIGNATURAS WHERE COD>5;
+	DBMS_OUTPUT.PUT_LINE('Numero de filas borrados'||TO_CHAR(SQL%ROWCOUNT));
+END;
+/
+
+
+CREATE OR REPLACE PROCEDURE CONTADOR()
+IS
+BEGIN 
+	DELETE FROM ASIGNATURAS WHERE COD>5;
+	DBMS_OUTPUT.PUT_LINE('Numero de filas borrados'||TO_CHAR(SQL%ROWCOUNT));
+END;
+/
+
 /* 10.	Obtener un bloque PL que introduciendo el código de un trabajador de la tabla EMPLE, visualizar el código y su salario para posteriormente actualizarlo en función de su sueldo. Si su sueldo es mayor de 1200 € su incremento será del 20% y su es menor del 25%. Visualizar su sueldo actualizado.
  */
+ 
+ 
+DECLARE
+	N_SALARIO NUMBER;
+BEGIN
+	SELECT SALARIO INTO N_SALARIO FROM EMPLE WHERE EMP_NO = TRAB;
+	IF N_SALARIO<1200 THEN
+	UPDATE EMPLE SET SALARIO = N_SALARIO + (N_SALARIO*0.25) WHERE EMP_NO = TRAB;
+	ELSE
+	UPDATE EMPLE SET SALARIO = N_SALARIO + (N_SALARIO*0.20) WHERE EMP_NO = TRAB;
+	END IF;
+	SELECT SALARIO INTO N_SALARIO FROM EMPLE WHERE EMP_NO = TRAB;
+	DBMS_OUTPUT.PUT_LINE(TRAB||' AHORA TIENE UN SALARIO DE '||N_SALARIO);
+END;
+/
+
+CREATE OR REPLACE PROCEDURE SUBIDA(TRAB NUMBER)
+IS
+	N_SALARIO NUMBER;
+BEGIN
+	SELECT SALARIO INTO N_SALARIO FROM EMPLE WHERE EMP_NO = TRAB;
+	IF N_SALARIO<1200 THEN
+	UPDATE EMPLE SET SALARIO = N_SALARIO + (N_SALARIO*0.25) WHERE EMP_NO = TRAB;
+	ELSE
+	UPDATE EMPLE SET SALARIO = N_SALARIO + (N_SALARIO*0.20) WHERE EMP_NO = TRAB;
+	END IF;
+	SELECT SALARIO INTO N_SALARIO FROM EMPLE WHERE EMP_NO = TRAB;
+	DBMS_OUTPUT.PUT_LINE(TRAB||' AHORA TIENE UN SALARIO DE '||N_SALARIO);
+END;
+/
+
 /* 11.	Introduciendo un número por teclado, correspondiente al dorsal de un futbolista, dar como salida el puesto en el que juega dicho jugador. Utilizar la estructura de control más adecuada.
  */
 /* 12.	Dados dos números introducidos por teclado, obtener cuál de los dos es mayor.
@@ -102,21 +253,50 @@ END;
 	DBMS_OUTPUT.PUT_LINE('El número de cifras del número introducido es de: '||V_CONTADOR);
  END;
  /
- 
+
+EXECUTE CONTADOR('712121');
  
 DECLARE
-	
- 
+	V_CONTADOR NUMBER;
+BEGIN
+	V_CONTADOR:=1..LENGTH(&COSA);
+	DBMS_OUTPUT.PUT_LINE('El número de cifras del número introducido es de: '||V_CONTADOR);
+END;
+/
+
  
 /* 14.	Dado un número introducido por teclado, visualizar por pantalla todos los números iguales o inferiores a él. Este programa se efectuará por todos los tipos de estructuras repetitivas.
  */
- 
+
+--EJEMPLO CON BUCLE FOR
 CREATE OR REPLACE PROCEDURE NUMEROS(COSA NUMBER)
 IS
-	V_CADENA 
+	V_COSA NUMBER;
 BEGIN
+	SELECT COSA + 1 INTO V_COSA FROM DUAL;
+	FOR I IN..A LOOP
+	V_COSA:= V_COSA - 1:
+	DBMS_OUTPUT.PUT_LINE(V_COSA);
+	END LOOP;
 END;
 /
+
+--EJEMPLO CON BUCLE WHILE
+
+CREATE OR REPLACE PROCEDURE NUMEROS2(COSA NUMBER)
+IS
+	V_COSA
+BEGIN
+	SELECT COSA + INTO V_COSA FROM DUAL;
+	WHILE V_COSA>1 LOOP
+	V_COSA:= V_COSA -1;
+	DBMS_OUTPUT.PUT_LINE(V_COSA);
+	END LOOP;
+END;
+/
+
+
+
 /* 15.	Introduciendo un año por teclado, decir si este es bisiesto o no.
  */
  
--- pl/t12/ejs_funciones_t12.sql
+++ pl/t12/ejs_funciones_t12.sql
@@ -1,6 +1,8 @@
 --15 Crear una función que sume dos números.
 
 CREATE OR REPLACE FUNCTION FUNCION_SUMAR(COSA1 NUMBER, COSA2 NUMBER)
+RETURN 
+	NUMBER
 IS
 	COSA_RESULTADO NUMBER;
 BEGIN
@@ -10,13 +12,58 @@ BEGIN
 END;
 /
 
+SELECT FUNCION_SUMAR() FROM DUAL;
 
 --16 Crear una función que visualice una cadena al revés.
 
+CREATE OR REPLACE FUNCTION AL_REVES(COSA VARCHAR)
+RETURN 
+	VARCHAR
+IS
+	ROBLE VARCHAR2(65);
+BEGIN
+	for i in reverse 1..length(COSA) LOOP
+		ROBLE:= ROBLE|| SUBSTR(COSA,i,1);
+	end loop;
+	dbms_output.put_line(ROBLE);
+	RETURN(ROBLE);
+END;
+/
+
+SELECT AL_REVES('MANOLO') FROM DUAL;
+
 /* 17 Escribe una función que reciba una fecha y devuelva el año, en número, correspondiente a esa fecha. */
 
+CREATE OR REPLACE FUNCTION DAR_FECHA(FECHA DATE)
+RETURN
+	NUMBER
+IS
+	aanyo number;
+BEGIN
+	aanyo:=TO_NUMBER(TO_CHAR(FECHA, 'YYYY'));
+	RETURN(aanyo);
+END;
+/
 
-/* 18-Escribir una función que devuelva el valor con IVA de una cantidad que se pasara como primer parámetro. La función podrá recoger un segundo parámetro, que será el tipo de IVA siendo el valor por defecto 16. /*
+select DAR_FECHA('24-06-2016') FROM DUAL;
+
+/* 18-Escribir una función que devuelva el valor con IVA de una cantidad que se pasara como primer parámetro. La función podrá recoger un segundo parámetro, que será el tipo de IVA siendo el valor por defecto 16. */
+
+CREATE OR REPLACE FUNCTION DAME_IVA(PRECIO NUMBER, tipo number default 16)
+RETURN
+	NUMBER;
+IS
+	FINAL NUMBER;
+BEGIN
+	FINAL:= PRECIO*TIPO;
+	-- RETURN(FINAL);
+	
+END;
+/
+
+SELECT DAME_IVA(500,20) FROM DUAL;
 
 /* 19- Utilizar la función anterior para obtener el salario de
 los empleados con el iva de la función. */
+
+SELECT EMP_NO, SALARIO, DAME_IVA(SALARIO) FROM EMPLE;
